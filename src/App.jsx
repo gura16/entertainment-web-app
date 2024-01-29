17dfb40da3bcdc57b24431c62790be76a98e8df9
guramikvakhadze <gkvaxadze@gmail.com>
2024-01-29 21:59:12 +0000

    add signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { createGlobalStyle } from "styled-components";
 import styled from "styled-components";
 import Login from "./login";
+import Signup from "./signup";
 function App() {
   return (
     <>
       <GlobalStyles />
-      <Login />
+      {/* <Login /> */}
+      <Signup />
     </>
   );
 }
